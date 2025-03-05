4e73d204724559233266bbf70e6df27b88500788
DBuchak <buchak_ak23@nuwm.edu.ua>
2025-03-05 17:54:50 +0200

    The index.js file implements sending the index.ejs file with HTML code in response when visiting the root(/) URL

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-//Підключаємо залежність express
 var express = require('express');
-//Викликаємо express
 var app = express();
-//Налаштовуємо маршрут root(/) URL-адреси
-app.get('/', function (req, res) {
-//При відвідуванні root(/) URL-адреси у відповідь 
-//отримаємо повідомлення "Привіт, мій друже!"
- res.send('Привіт, мій друже!');
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+    res.render("index");
 });
-//Налаштовуємо сервер для прослуховування порту 3000
 app.listen(3000, function() {
     console.log("Сервер працює на порту 3000!");
 });
